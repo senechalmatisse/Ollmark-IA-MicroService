4002dc793e72e176a4a7068c1e10e716099c6092
novelastre <suzuco123456@gmail.com>
2026-03-10 11:12:44 +0100

    Structure fichié problématique.

--- src/main/java/com/penpot/ai/application/advisor/MissingInformationAdvisor.java
+++ src/main/java/com/penpot/ai/application/advisor/MissingInformationAdvisor.java
@@ -1,4 +1,4 @@
-﻿package com.penpot.ai.application.advisor;
+package com.penpot.ai.application.advisor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClientRequest;
